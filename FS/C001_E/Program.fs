702e92d86b51587a9203b17af3519b52874ee42d
Andrew Benavides <andrew.benavides@gmail.com>
2014-01-25 15:59:59 -0500

    Added simple solution for Challenge #1 Easy [C001_E]

---
+++ FS/C001_E/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let prompt message =
+    printf "%s " message
+    Console.ReadLine()
+
+let name = prompt "What's your name?"
+let age = prompt "How old are you?"
+let username = prompt "What's your reddit user name?"
+
+let output =
+    sprintf "Your name is %s, you are %s years old, and your username is %s." name age username
+
+let Pause() = 
+    Console.ReadLine() |> ignore
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%s" output
+    Pause()
+    0 // return an integer exit code
