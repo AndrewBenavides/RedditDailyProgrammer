fcb7e70bae83d0520865c79681e795106319287e
Andrew Benavides <andrew.benavides@pb.com>
2014-01-30 14:08:23 -0500

    Added Decrypt to [C002_E], refactored list management
      - Added charList type to manage 'original' and 'shifted' lists
      - characters, numbers, and punctuation are charLists that replace their
        valid_ and shifted_ counterparts
      - Moved GetShiftedValue out of EncryptMessage into CaesarCipher scope
      - Replaced GetShifted_ functions in EncryptMessage with Encrypt function that
        takes charList parameter

--- FS/C003_E/Program.fs
+++ FS/C003_E/Program.fs
@@ -1,4 +1,6 @@
-﻿type CaesarCipher(characterShift: int, ?numberShift: int, ?punctuationShift: int) = 
+﻿type charList = {original: List<char>; shifted: List<char>;}
+
+type CaesarCipher(characterShift: int, ?numberShift: int, ?punctuationShift: int) = 
     let GetShiftedHead lst shift  = 
         let len = List.length lst
         let rec MapShiftWithinLength s = 
@@ -11,35 +13,44 @@
     let ShiftList lst shift =
         let a, b = List.partition (fun c -> c >= (GetShiftedHead lst shift)) lst
         List.append a b
+    
+    let buildCharList lst shift = 
+        { original = lst; shifted = ShiftList lst shift; }
 
-    let validCharacters = ['A'..'Z']
-    let validNumbers = ['0'..'9']
-    let validPunctuation = ['!'..'~'] |> List.filter(fun c-> (System.Char.IsPunctuation(c)))
+    let characters = buildCharList ['A'..'Z'] characterShift
+    let numbers = buildCharList ['0'..'9'] (defaultArg numberShift 0)
+    let punctuation = buildCharList (['!'..'~'] |> List.filter(fun c-> (System.Char.IsPunctuation(c)))) (defaultArg punctuationShift 0)
 
-    let shiftedCharacters = ShiftList validCharacters characterShift
-    let shiftedNumbers = ShiftList validNumbers (defaultArg numberShift 0)
-    let shiftedPunctuation = ShiftList validPunctuation (defaultArg punctuationShift 0)
+    let GetShiftedValue sourceList destinationList c =
+        match List.tryFindIndex(fun e -> e = c) sourceList with
+        | Some index -> List.nth destinationList index
+        | None -> c
 
     member this.EncryptMessage (message: string) =
         let upperMessage = message.ToUpper()
 
-        let GetShiftedValue validList shiftedList c =
-            match List.tryFindIndex(fun e -> e = c) validList with
-            | Some index -> List.nth shiftedList index
-            | None -> c
-
-        let GetShiftedCharacter c = GetShiftedValue validCharacters shiftedCharacters c
-        let GetShiftedNumber c = GetShiftedValue validNumbers shiftedNumbers c
-        let GetShiftedPunctuation c = GetShiftedValue validPunctuation shiftedPunctuation c
+        let Encrypt (lst: charList) c =
+            GetShiftedValue lst.original lst.shifted c
         
-        String.map GetShiftedCharacter upperMessage 
-        |> String.map GetShiftedNumber 
-        |> String.map GetShiftedPunctuation
+        String.map (Encrypt characters) upperMessage 
+        |> String.map (Encrypt numbers) 
+        |> String.map (Encrypt punctuation)
+
+    member this.DecryptMessage (message: string) =
+        let upperMessage = message.ToUpper()
+
+        let Decrypt (lst: charList) c =
+            GetShiftedValue lst.shifted lst.original c
+
+        String.map (Decrypt characters) upperMessage 
+        |> String.map (Decrypt numbers) 
+        |> String.map (Decrypt punctuation)
 
 [<EntryPoint>]
 let main argv = 
     let cipher = new CaesarCipher(2,-2,3)
     printfn "%s" (cipher.EncryptMessage "test! 1234?")
+    printfn "%s" (cipher.DecryptMessage "YJGP YKNN VJG DCPPGTU CPF XKEVQTA RCTCFGU EGNGDTCVG VJG FCA C DGVVGT YQTNF YCU YQP\\")
     System.Console.ReadLine() |> ignore
     printfn "%A" argv
     0 // return an integer exit code
