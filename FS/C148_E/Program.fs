e886686315c22d1f2947a3fa2ddf674de28cf72b
Andrew Benavides <andrew.benavides@pb.com>
2014-01-31 18:48:31 -0500

    Expanded solution for [C148_E]
      - Added ICombinationLock interface
      - Implemented 5outh's and ooesili's mathmatical solution as CombinationLock2
      - Refactored CombinationLock3 as hybrid between CombinationLock1 and 5outh's
        and ooesili's solution. Allows for locks with more than 3 movements.
      - Implemented PrintLockInfo function to write message to console

--- FS/C148_E/Program.fs
+++ FS/C148_E/Program.fs
@@ -10,7 +10,13 @@
         let movements = inputs.Tail
         ticks, movements
 
-type CombinationLock(str: string) =
+type ICombinationLock =
+    interface
+    abstract member Input: string
+    abstract member Clicks: int
+    end
+
+type CombinationLock1(str: string) = 
     let ticks, movements = InputParser.Parse(str)
     
     let Clockwise dst (acc, src) =
@@ -35,53 +41,94 @@ type CombinationLock(str: string) =
         Spin acc
         |> CounterClockwise (snd elem)
 
-    member this.Counter =
-        let ApplyMovementIndex index elem = (index, elem)
-        let HasEvenIndex elem = (fst elem % 2 = 0)
+    interface ICombinationLock with
+        member this.Input = str
+        member this.Clicks =
+            let ApplyMovementIndex index elem = (index, elem)
+            let HasEvenIndex elem = (fst elem % 2 = 0)
         
-        let indexedMovements = Seq.mapi ApplyMovementIndex ([0] @ movements)
+            let indexedMovements = Seq.mapi ApplyMovementIndex ([0] @ movements)
         
-        let TurnDialAccumulateClicks acc elem =
-            match elem with
-            | (index, _) when index = 0 -> Clockwise (snd elem) acc
-            | (index, _) when index = List.length movements && HasEvenIndex elem -> CounterClockwise (snd elem) acc
-            | (index, _) when index = List.length movements -> Clockwise (snd elem) acc
-            | _ when HasEvenIndex elem -> SpinCounterClockwise acc elem
-            | _ -> SpinClockwise acc elem
+            let TurnDialAccumulateClicks acc elem =
+                match elem with
+                | (index, _) when index = 0 -> Clockwise (snd elem) acc
+                | (index, _) when index = List.length movements && HasEvenIndex elem -> CounterClockwise (snd elem) acc
+                | (index, _) when index = List.length movements -> Clockwise (snd elem) acc
+                | _ when HasEvenIndex elem -> SpinCounterClockwise acc elem
+                | _ -> SpinClockwise acc elem
 
-        (*  The first movement is executed as goto 0 from 0, or a full spin. 
+            (*  The first movement is executed as goto 0 from 0, or a full spin. 
             
-            Before any intermediate movements are made, a preliminary full spin is executed.
-            The movement direction is determined by whether or not the movement index is even or odd. 
-            i.e. The 0th movement is even and counter-clockwise, the 1st movement is odd and clockwise.
+                Before any intermediate movements are made, a preliminary full spin is executed.
+                The movement direction is determined by whether or not the movement index is even or odd. 
+                i.e. The 0th movement is even and counter-clockwise, the 1st movement is odd and clockwise.
             
-            The final movement is executed as a direct spin to the final destination without a preliminary full spin.
-            The movement direction is determined by whether or not the movement index is even or odd.
-        *)
+                The final movement is executed as a direct spin to the final destination without a preliminary full spin.
+                The movement direction is determined by whether or not the movement index is even or odd.
+            *)
 
-        let clicks = fst (Seq.fold TurnDialAccumulateClicks (0,0) indexedMovements)
-        clicks
+            let clicks = fst (Seq.fold TurnDialAccumulateClicks (0,0) indexedMovements)
+            clicks
                 
-type CombinationLock2() =
+type CombinationLock2(str: string) =
+    let ticks, movements = InputParser.Parse(str)
+
     let f n x y z = 
         let g a b = (n + b - a - 1) % n + 1 //(n + b - a - 1) gets the remainder, like Haskell's mod function appears to do
         3 * n + x + g y x + g y z
 
-    let a = f 5 1 2 3
+    interface ICombinationLock with
+        member this.Input = str
+        member this.Clicks =
+            f ticks movements.[0] movements.[1] movements.[2]
 
-type CombinationLock3() =
-    let ticks = 5
-    let move src dst = (ticks + dst - src - 1) % ticks + 1
-    let spin = move 0 0
-    let cw src dst = move src dst
-    let ccw src dst = move dst src
+type CombinationLock3(str: string) =
+    let ticks, movements = InputParser.Parse(str)
 
-    let a = spin + spin + cw 0 1 + spin + ccw 1 2 + cw 2 3
+    let Move src dst = (ticks + dst - src - 1) % ticks + 1
+    //  g a b = (n + b - a - 1) % n + 1
+    let Spin = Move 0 0
+    // 3 * n, as above in CombinationLock2, if executed 3 times will return 3 * ticks
+    let Clockwise src dst = Move src dst
+    // g y z, as above in CombinationLock2, a clockwise movement
+    let CounterClockwise src dst = Move dst src
+    // g y x, as above in CombinationLock2, a counter-clockwise movement
+    // if ticks was set to 5 then we would find that 21 = spin + spin + cw 0 1 + spin + ccw 1 2 + cw 2 3
+    let SpinClockwise src dst = Spin + Clockwise src dst
+    let SpinCounterClockwise src dst = Spin + CounterClockwise src dst
+    
+    interface ICombinationLock with
+        member this.Input = str
+        member this.Clicks =
+            let ApplyMovementIndex index elem = (index, elem)
+            let HasEvenIndex elem = (fst elem % 2 = 0)
+        
+            let indexedMovements = List.mapi ApplyMovementIndex ([0] @ movements)
+        
+            let TurnDialAccumulateClicks acc elem =
+                let src = if fst elem > 0 then snd (indexedMovements.[fst elem - 1]) else 0
+                let dst = snd elem
+                let clicks =
+                    match elem with
+                    | (index, _) when index = 0 -> Clockwise src dst
+                    | (index, _) when index = List.length movements && HasEvenIndex elem -> CounterClockwise src dst
+                    | (index, _) when index = List.length movements -> Clockwise src dst
+                    | _ when HasEvenIndex elem -> SpinCounterClockwise src dst
+                    | _ -> SpinClockwise src dst
+                acc + clicks
+
+            let clicks = List.fold TurnDialAccumulateClicks 0 indexedMovements
+            clicks
+    
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    let a = new CombinationLock("5 1 2 3")
-    let b = a.Counter
-    //Console.ReadLine() |> ignore
+    let PrintLockInfo (lock: ICombinationLock) lockType =
+        printfn "Number of clicks for input \"%s\" using %s was %d" lock.Input lockType lock.Clicks |> ignore
+
+    PrintLockInfo (new CombinationLock1("5 1 2 3")) "CombinationLock1"
+    PrintLockInfo (new CombinationLock2("5 1 2 3")) "CombinationLock2"
+    PrintLockInfo (new CombinationLock3("5 1 2 3")) "CombinationLock3"
+    PrintLockInfo (new CombinationLock3("5 1 2 3 2")) "CombinationLock3"
+    PrintLockInfo (new CombinationLock3("60 12 32 48 1")) "CombinationLock3"
     0 // return an integer exit code
