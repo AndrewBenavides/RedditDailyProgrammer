c89bb73563a062b6745ce2b37f2dcf176cbfb41e
Andrew Benavides <andrew.benavides@gmail.com>
2014-02-14 14:22:59 -0500

    Cleaned up code for [C145_E], added InputParser

--- FS/C145_E/Program.fs
+++ FS/C145_E/Program.fs
@@ -1,42 +1,54 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
 
 type TreeBuilder(maxWidth: int, trunkChar:char, leafChar: char) =
     let rand = System.Random()
     let branchWidths = {1..2..maxWidth}
 
-    let leafBuilder i =
-        let index = i + 1
-        let r = rand.Next(2, maxWidth)
-        let leafChar = if index % r = 0 then 'o' else leafChar
+    let leafBuilder index =
+        let leafChar = 
+            let r = rand.Next(2, maxWidth)
+            if index % r = 0 then 'o' else leafChar
+
         sprintf "%c" leafChar
 
-    let padString width func =
-        let padding = (maxWidth - width) / 2
+    let layerBuilder leafCount func =
+        let padding = (maxWidth - leafCount) / 2
         String.init maxWidth (fun initIndex ->
             let index = initIndex + 1
             match index with
             | index when index <= padding -> " "
-            | index when index > padding + width -> " "
+            | index when index > padding + leafCount -> " "
             | index -> func index
         )
 
     let branchBuilder width =
-        padString width leafBuilder
+        layerBuilder width leafBuilder
 
-    let baseBuilder =
-        padString 3 (fun i -> sprintf "%c" trunkChar)
+    let trunkBuilder =
+        layerBuilder 3 (fun i -> sprintf "%c" trunkChar)
 
     let branches = Seq.map branchBuilder branchWidths
 
-    member this.Print =
+    member this.Print() =
         for branch in branches do printfn "%s" branch
-        printfn "%s" baseBuilder
+        printfn "%s" trunkBuilder
 
+type InputParser(input: string) =
+    let args = input.Split(' ')
+    
+    member this.MakeTree() =
+        try
+            let maxWidth = Int32.Parse(args.[0])
+            let trunkChar = Char.Parse(args.[1])
+            let leafChar = Char.Parse(args.[2])
+            let tree = new TreeBuilder(maxWidth, trunkChar, leafChar)
+            tree.Print()
+        with
+            | _ -> printfn "Input was not valid."
 
 [<EntryPoint>]
 let main argv = 
-    let tree = new TreeBuilder(15, '#', '*')
-    tree.Print
+    InputParser("3 # *").MakeTree()
+    InputParser("13 = +").MakeTree()
     System.Console.ReadLine() |> ignore
     0 // return an integer exit code
