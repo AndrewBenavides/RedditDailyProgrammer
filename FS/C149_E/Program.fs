5f82071bff34a2e6325776f2b207cffc8a01d7c8
Andrew Benavides <andrew.benavides@gmail.com>
2014-02-24 13:26:17 -0500

    Work in progress for [C149_E]
      - seq<char> is not compatible with String.concat, seeking alternate solutions

--- FS/C149_E/Program.fs
+++ FS/C149_E/Program.fs
@@ -1,5 +1,35 @@
 ﻿
+let IsVowel c =
+    match c with
+    | 'a' | 'e' | 'i' | 'o' | 'u' -> true
+    | _ -> false
+
+let IsConsonant c =
+    match c with
+    | ' ' -> false
+    | _ when IsVowel c -> false
+    | _ -> true
+
+let Disemvowel str = 
+    let FindVowels = Seq.filter IsVowel
+    let FindConsonants = Seq.filter IsConsonant
+
+    (FindConsonants str, FindVowels str)
+
+
+let OutputSequence title (charSeq: seq<char>) =
+    printfn "%10s: %s" title (String.concat "" charSeq)
+
+let PrintDisemvoweledSequences input =
+    let consonants, vowels = Disemvowel input
+    OutputSequence "Consonants" consonants
+    OutputSequence "Vowels" vowels
+    printfn ""
+
+let Print
+
 [<EntryPoint>]
 let main argv = 
+    //let test = "two drums and a cymbal fall off a cliff"
     printfn "%A" argv
     0 // return an integer exit code
