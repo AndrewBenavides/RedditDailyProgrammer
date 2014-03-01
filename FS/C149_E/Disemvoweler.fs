420c26c575ae94cb04028d4793fca2d77a6eac01
Andrew Benavides <andrew.benavides@gmail.com>
2014-02-28 23:02:52 -0500

    [C149_E] Refactored Disemvoweler to use F# Active Patterns
    - Replaced IsVowel function with Active Pattern
    - Replaced List.partition with a Seq.fold that uses the Active Pattern
    - Charlists are constructed backwards and are reversed via pipeline
    
    More code, but makes use of F# features.

--- FS/C149_E/Disemvoweler.fs
+++ FS/C149_E/Disemvoweler.fs
@@ -1,10 +1,19 @@
-﻿let IsVowel letter = Seq.exists ((=) letter) "aeiou"
+﻿let (|Vowel|Consonant|Space|) c =
+    if Seq.exists ((=) c) "aeiouAEIOU" then Vowel
+    elif c = ' ' then Space
+    else Consonant
 
 let Stringify charlist = Seq.fold (fun s c -> s + string c) "" charlist
 
 let Disemvoweler (input: string) =
-    let chars = List.ofSeq (input.ToLower().Replace(" ",""))
-    let vowels, consonants = List.partition (fun e -> IsVowel e) chars
+    let vowels, consonants = 
+        Seq.fold (fun (vow, con) c ->
+            match c with
+            | Vowel -> c::vow, con
+            | Consonant -> vow, c::con
+            | Space -> vow, con
+        ) ([],[]) input
+        |> fun (vow, con) -> List.rev vow, List.rev con //pattern matching on pipelined values
     Stringify consonants, Stringify vowels
 
 let Disemvowel (input: string) = 
@@ -16,5 +25,5 @@ let main argv =
     Disemvowel "two drums and a cymbal fall off a cliff"
     Disemvowel "all those who believe in psychokinesis raise my hand"
     Disemvowel "did you hear about the excellent farmer who was outstanding in his field"
-    //System.Console.ReadLine() |> ignore
+    System.Console.ReadLine() |> ignore
     0 // return an integer exit code
