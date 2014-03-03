b6ecfb179ea724ec7febc02b01bc4e024f0e63ac
Andrew Benavides <andrew.benavides@gmail.com>
2014-03-03 13:39:57 -0500

    [C150_I] Changed EnabledWords to load a reversed/sorted enabled words file.

--- CS/C150_I/EnabledWords.cs
+++ CS/C150_I/EnabledWords.cs
@@ -6,23 +6,18 @@ using System.Threading.Tasks;
 
 namespace C151_I {
     public static class EnabledWords {
-        private static string _enabledWordsFilePath = ".\\enable1.txt";
-        private static string[] _enabledWords = GetReversedArray();
+        private static string _reverseEnabledWordsFilePath = ".\\enable1_rev.txt";
+        private static string[] _reverseEnabledWords = GetReversedArray();
 
         private static string[] GetReversedArray() {
             //http://stackoverflow.com/a/500930
-            var list = new SortedList<string, string>();
-            foreach (var line in ReadLines()) {
-                list.Add(line.Reverse2(), line);
-            }
-            var array = list.Select(kvp => kvp.Key).ToArray();
-            return array;
+            return ReadLines().ToArray();
         }
 
         public static bool Contains(string str) {
             var rev = str.Reverse2();
-            var nearest = Array.BinarySearch(_enabledWords, rev);
-            if (nearest > -1 || _enabledWords[Math.Abs(nearest) - 1].Contains(rev)) {
+            var nearest = Array.BinarySearch(_reverseEnabledWords, rev);
+            if (nearest > -1 || _reverseEnabledWords[Math.Abs(nearest) - 1].Contains(rev)) {
                 return true;
             } else {
                 return false;
@@ -30,11 +25,11 @@ namespace C151_I {
         }
 
         public static bool Matches(string str) {
-            return Array.BinarySearch(_enabledWords, str.Reverse2()) > -1 ? true : false;
+            return Array.BinarySearch(_reverseEnabledWords, str.Reverse2()) > -1 ? true : false;
         }
 
         private static IEnumerable<string> ReadLines() {
-            using (var reader = new System.IO.StreamReader(_enabledWordsFilePath)) {
+            using (var reader = new System.IO.StreamReader(_reverseEnabledWordsFilePath)) {
                 while (!reader.EndOfStream) {
                     yield return reader.ReadLine();
                 }
