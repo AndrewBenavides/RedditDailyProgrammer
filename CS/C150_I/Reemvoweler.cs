d778bb53c618a1967459829011b912254e93ec38
Andrew Benavides <andrew.benavides@gmail.com>
2014-03-03 08:49:19 -0500

    [C150_I] Work in progress, now finding at least one word in the phrase.

--- CS/C150_I/Reemvoweler.cs
+++ CS/C150_I/Reemvoweler.cs
@@ -9,8 +9,12 @@ using System.Threading.Tasks;
 namespace C151_I {
     class Program {
         static void Main(string[] args) {
-            //var rev = new Reemvoweler("wwllfndffthstrds", "eieoeaeoi");
-            Console.WriteLine(EnabledWords.Matches("asteroids").ToString());
+            var con = "wwllfndffthstrds";
+            var vow = "eieoeaeoi";
+
+            var word = new Word("", new Stack<char>(con), new Stack<char>(vow));
+            var matches = word.SubPartialMatches.ToList();
+            var fullMatches = word.SubMatches.ToList();
         }
 
 
@@ -49,43 +53,69 @@ namespace C151_I {
 
     }
 
+    public static class StackExtensions {
+        public static Stack<T> Clone<T>(this Stack<T> source) {
+            return new Stack<T>(source.Reverse());
+        }
+    }
+
     public class Word {
         private string _word;
-        public bool IsPartiallyFound { get; private set; }
-        public bool IsFound { get; private set; }
+        public bool IsPartialMatch { get; private set; }
+        public bool IsMatch { get; private set; }
         public Stack<char> RemainingConsonants { get; private set; }
         public Stack<char> RemainingVowels { get; set; }
+        public int MatchingLength { get { return this.IsMatch ? _word.Length : 0; } }
 
         public Word NextConsonant { get; private set; }
         public Word NextVowel { get; private set; }
+        public IEnumerable<Word> SubPartialMatches { get { return this.GetSubPartialMatches(); } }
+        public IEnumerable<Word> SubMatches { get { return this.GetSubMatches(); } }
 
         public Word(string word, Stack<char> remainingConsonants, Stack<char> remainingVowels) {
             _word = word;
             this.RemainingConsonants = remainingConsonants;
             this.RemainingVowels = remainingVowels;
-            this.IsPartiallyFound = EnabledWords.Contains(_word);
-            this.IsFound = this.IsPartiallyFound ? EnabledWords.Matches(_word) : false;
-            this.NextConsonant = GetNextConsonant(this.RemainingConsonants);
-            this.NextVowel = GetNextVowel(this.RemainingVowels);
+            this.IsPartialMatch = EnabledWords.Contains(_word);
+            this.IsMatch = this.IsPartialMatch ? EnabledWords.Matches(_word) : false;
+            this.NextConsonant = GetNextWordWith(this.RemainingConsonants.Clone());
+            this.NextVowel = GetNextWordWith(this.RemainingVowels.Clone());
+            //this.SubMatches = GetSubMatches();
         }
 
-        private Word GetNextConsonant(Stack<char> consonants) {
-            if (this.IsPartiallyFound) {
-                var word = consonants.Any() ? consonants.Pop() + _word : _word;
-                var next = new Word(word, consonants, this.RemainingVowels);
+        private Word GetNextWordWith(Stack<char> charStack) {
+            if(this.IsPartialMatch) {
+                var c = charStack.Any() ? charStack.Pop() : char.MinValue;
+                var word = c + _word;
+                Word next;
+                if ("aeiouAEIOU".Contains(c)) {
+                    next = new Word(word, this.RemainingConsonants.Clone(), charStack);
+                } else {
+                    next = new Word(word, charStack, this.RemainingVowels.Clone());
+                }
                 return next;
             } else {
                 return null;
             }
         }
 
-        private Word GetNextVowel(Stack<char> vowels) {
-            if (this.IsPartiallyFound) {
-                var word = vowels.Any() ? vowels.Pop() + _word : _word;
-                var next = new Word(word, this.RemainingConsonants, vowels);
-                return next;
-            } else {
-                return null;
+        private IEnumerable<Word> GetSubPartialMatches() {
+            yield return this;
+            if (this.NextConsonant != null) {
+                foreach (var match in this.NextConsonant.SubPartialMatches) { yield return match; }
+            }
+            if (this.NextVowel != null) {
+                foreach (var match in this.NextVowel.SubPartialMatches) { yield return match; }
+            }
+        }
+
+        private IEnumerable<Word> GetSubMatches() {
+            if (this.IsMatch) yield return this;
+            if (this.NextConsonant != null) {
+                foreach (var match in this.NextConsonant.SubMatches) { yield return match; }
+            }
+            if (this.NextVowel != null) {
+                foreach (var match in this.NextVowel.SubMatches) { yield return match; }
             }
         }
 
@@ -100,83 +130,4 @@ namespace C151_I {
 
         }
     }
-
-    //class Reemvoweler {
-    //    private string con;
-    //    private string vow;
-    //    Stack conStack;
-    //    Stack vowStack;
-
-    //    public Reemvoweler(string consonants, string vowels) {
-    //        con = consonants;
-    //        vow = vowels;
-    //        conStack = new Stack(con.ToList());
-    //        vowStack = new Stack(vow.ToList());
-    //        FindWords();
-    //    }
-
-    //    public bool IsInEnabledWords(string str) {
-    //        var found = false;
-    //        //while (!found) { }
-    //        foreach (var line in ReadLines()) {
-    //            if (str.Length <= line.Length) {
-    //                if (line.Substring(line.Length - str.Length) == str) {
-    //                    found = true;
-    //                    break;
-    //                }
-    //            }
-    //        }
-    //        return found;
-    //    }
-
-    //    public bool IsEntireWord(string str) {
-    //        foreach (var line in ReadLines()) {
-    //            if (line == str) {
-    //                return true;
-    //            }
-    //        }
-    //        return false;
-    //    }
-
-    //    public bool IsVowel(char letter) {
-    //        return "aeiouAEIOU".Contains(letter);
-    //    }
-
-    //    public IEnumerable<string> ReadLines() {
-    //        var reader = new System.IO.StreamReader(".\\enable1.txt");
-    //        while (!reader.EndOfStream) {
-    //            yield return reader.ReadLine();
-    //        }
-    //    }
-
-    //    public string FindWords() {
-    //        var word = string.Empty;
-    //        var phrase = string.Empty;
-    //        var takeVowel = false;
-    //        char currentLetter;
-    //        while (conStack.Count > 0 || vowStack.Count > 0) {
-    //            currentLetter = (char)(!takeVowel ? conStack.Pop() : vowStack.Pop());
-    //            word = currentLetter + word;
-    //            if (IsInEnabledWords(word)) {
-    //                if (IsEntireWord(word)) {
-    //                    phrase = word + " " + phrase;
-    //                    word = "";
-    //                    takeVowel = false;
-    //                } else {
-    //                    takeVowel = false;
-    //                }
-    //            } else {
-    //                word = word.Substring(1);
-    //                if (IsVowel(currentLetter)) {
-    //                    vowStack.Push(currentLetter);
-    //                    takeVowel = false;
-    //                } else {
-    //                    conStack.Push(currentLetter);
-    //                    takeVowel = true;
-    //                }
-    //            }
-    //        }
-    //        return phrase;
-    //    }
-    //}
 }
