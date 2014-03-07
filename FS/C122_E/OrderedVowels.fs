3871d7d37bed03751e18a574bc3629222f4d8ab5
Andrew Benavides <andrew.benavides@gmail.com>
2014-03-06 20:23:41 -0500

    [C122_E] Added solution.

--- FS/C122_E/OrderedVowels.fs
+++ FS/C122_E/OrderedVowels.fs
@@ -1,3 +1,30 @@
-﻿[<EntryPoint>]
+﻿// A short experiment with long function names...
+let EnabledWords = seq {
+    use streamReader = new System.IO.StreamReader(".\enable1.txt")
+    while not streamReader.EndOfStream do yield streamReader.ReadLine().ToLower()
+}
+
+let IsVowel c = Seq.exists ((=) c) "aeiouy"
+
+let VowelsOnlyFrom c =  if IsVowel c then string c else ""
+
+let VowelsAreSequentialIn word =
+    let vowels = String.collect VowelsOnlyFrom word
+    vowels = "aeiouy"
+
+let WordsWithSequentialVowels =
+    Seq.where VowelsAreSequentialIn EnabledWords
+
+let HighlightVowelsIn word =
+    String.map (fun c -> if IsVowel c then char (int c - 32) else c) word
+
+let PrintWordsIn word =
+    printfn "%s" (HighlightVowelsIn word)
+
+[<EntryPoint>]
 let main argv = 
+    printfn "Words with one of each vowel in sequential order:"
+    Seq.iter PrintWordsIn WordsWithSequentialVowels
+    printfn "\nSequence complete. Press enter to exit."
+    System.Console.ReadLine() |> ignore
     0 // return an integer exit code
