47901302e629d82fa1a36553d79e5f8074fe318c
Andrew Benavides <andrew.benavides@gmail.com>
2014-03-09 12:49:47 -0400

    [C147_E] Fixed "coin change" process, validated input. Needs refactoring.

--- FS/C147_E/SportsPoints.fs
+++ FS/C147_E/SportsPoints.fs
@@ -4,11 +4,14 @@ let IsInvalidScore score =
     (score >= 0) && (List.exists ((=) score) [1;2;4;5;])
 
 let FindCombinations score = 
-    let rec find remaining allowed accumulated =
-        if remaining > 0 then
+    let rec find remaining allowed (accumulated: List<int>)=
+        if List.isEmpty allowed then
+            []
+        elif remaining > 0 then
             let invalid, valid = List.partition (fun i -> i > remaining) allowed
-            if (List.length valid = 0 && remaining > 0) then 
-                []
+            if (List.length valid = 0 && remaining > 0) then
+                let highest = List.max invalid
+                find (remaining + highest) allowed.Tail accumulated.Tail
             else
                 let amount = List.max valid
                 let next = amount :: accumulated
@@ -18,12 +21,32 @@ let FindCombinations score =
     let rec reduce lst acc =
         match lst with
         | _ :: tl -> reduce tl ((find score lst []) :: acc)
-        | [] -> acc
+        | [] -> List.filter (fun (lst : List<int>) -> lst.Length > 0) (List.rev acc)
 
     reduce scores []
-        
-    
+
+let PrintCombinations score =
+    List.iteri (fun index scores -> 
+        printf "%i. " (index + 1)
+        List.iter (fun point -> 
+            printf "%d " point
+        ) scores
+        printfn ""
+    ) (FindCombinations score)
+
+let Process score =
+    if IsInvalidScore score then
+        printfn "Score %i is invalid." score
+    else
+        printfn "Score %i is valid. Possible combinations:" score
+        PrintCombinations score
+        printfn ""
+
 [<EntryPoint>]
 let main argv = 
-    let a = FindCombinations 18
+    printf "Input a score to validate: "
+    let success, score = System.Int32.TryParse(System.Console.ReadLine())
+    Process score
+    printfn "Press enter to exit."
+    System.Console.ReadLine() |> ignore
     0 // return an integer exit code
