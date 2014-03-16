fb3c469ed498dee9b682dd4da98b2127c276288d
Andrew Benavides <andrew.benavides@gmail.com>
2014-03-16 16:03:37 -0400

    [C146_E] Work in progress.

--- TS/C146_E/src/polygon.ts
+++ TS/C146_E/src/polygon.ts
@@ -1,1 +1,21 @@
-﻿
+﻿function processForm(form: HTMLFormElement) {
+    var sides = getValueAsNumber("sides");
+    var length = getValueAsNumber("length");
+    var perimeter = calculatePermiter(sides, length);
+    setPerimeter(perimeter);
+}
+
+function getValueAsNumber(id: string) {
+    var element = <HTMLInputElement>document.getElementById(id);
+    var value = element.valueAsNumber;
+    return value;
+}
+
+function setPerimeter(value: number) {
+    document.getElementById("perimeter").nodeValue = value.toString();
+}
+
+function calculatePermiter(sides: number, length: number): number {
+    var perimeter = sides * length;
+    return perimeter;
+}
