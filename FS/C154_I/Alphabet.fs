13c20f9bb1d5e89b0885e87918d3c6bd8f4dff10
Andrew Benavides <andrew.benavides@gmail.com>
2014-03-20 20:47:05 -0400

    [C154_I] Work in progress.

---
+++ FS/C154_I/Alphabet.fs
@@ -0,0 +1,41 @@
+﻿let alphabet (str: string) =
+    List.mapi (fun i c -> 
+        (c, i)
+    ) (List.ofSeq str)
+
+let weight (c: char) (alphabet: List<char * int>) =
+    List.pick (fun pairs -> 
+        match pairs with
+        | (chr, i) when chr = c ->  Some i
+        | _ -> None
+    ) alphabet
+
+let rec sorter a b alphabet =
+    let weightOf c = weight c alphabet
+    if a = [] then 1
+    elif b = [] then -1
+    else
+        let wa = weightOf (List.head a)
+        let wb = weightOf (List.head b)
+        if wa > wb then 1
+        elif wa < wb then -1
+        else sorter a.Tail b.Tail alphabet
+
+let sort a b alphabet =
+    sorter (List.ofSeq a) (List.ofSeq b) alphabet
+
+[<EntryPoint>]
+let main argv = 
+    let a = alphabet "UVWXYZNOPQRSTHIJKLMABCDEFG"
+    let words = [
+        "ANTLER";
+        "ANY";
+        "COW";
+        "HILL";
+        "HOW";
+        "HOWEVER";
+        "WHATEVER";
+        "ZONE";
+        ]
+    let s = sort "ZONE" "WHATEVER" a
+    0 // return an integer exit code
