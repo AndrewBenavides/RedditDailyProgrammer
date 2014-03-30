de47f8d49a67745c0918d62ec228f7f4d555e47b
Andrew Benavides <andrew.benavides@gmail.com>
2014-03-30 17:29:37 -0400

    [C153_E] Work in progress.

--- FS/C153_E/Pascal.fs
+++ FS/C153_E/Pascal.fs
@@ -29,10 +29,28 @@ let calcLayer n =
         calcRow n l
     ) [1 .. (n + 1)]
 
+let printlayer n =
+    let layer = calcLayer n
+    
+    let findLongest =
+        calcCell n (n / 2 / 2) (n / 2 / 2)
+//        let row = (List.length layer) - (int (ceil (float (n + 1) / 2.0))) + 1
+//        let rowLen = float (List.length layer.[row])
+//        //let cell = int (System.Math.Round((rowLen / 2.0),0, System.MidpointRounding.))
+//        let cell = int (ceil (rowLen / 2.0))
+//        layer.[row].[cell]
+    
+    findLongest
+
 [<EntryPoint>]
 let main argv = 
-    Seq.iter (fun layer -> 
-        Seq.iter (printf "%A ") layer
-        printfn ""
-    ) (calcLayer 5)
+//    Seq.iter (fun layer -> 
+//        Seq.iter (printf "%A ") layer
+//        printfn ""
+//    ) (calcLayer 5)
+    //let a = printlayer 1
+    //let b = printlayer 2
+    let c = printlayer 3
+    let d = printlayer 4
+    let e = printlayer 5
     0 // return an integer exit code
