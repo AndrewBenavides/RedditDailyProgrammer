2f332deb3fa600877d8b2fe21d64672877f7a9b1
Andrew Benavides <andrew.benavides@gmail.com>
2014-03-31 12:48:59 -0400

    [C153_E] Fixed printLayer function
    - printLayer finds highest/longest value and pads all other numbers to that
      length
    - Need to find way to print pyramid in a center-oriented fashion

--- FS/C153_E/Pascal.fs
+++ FS/C153_E/Pascal.fs
@@ -4,7 +4,6 @@ open Microsoft.FSharp.Math
 let factorials = new Dictionary<int, bigint>();
 
 let factorial (i: int) =
-    //Seq.fold (*) 1I [1I .. (bigint i)]
     let factorial n = Seq.fold (*) 1I [1I .. n]
     if factorials.ContainsKey i then factorials.[i]
     else
@@ -12,45 +11,41 @@ let factorial (i: int) =
         factorials.Add(i, fact)
         fact
 
-let calcCell n x y =
+let calc n x y z = 
     let fn = factorial n
-    let fx = factorial (n - x - y)
-    let fy = factorial (y)
-    let fz = factorial (x)
+    let fx = factorial x
+    let fy = factorial y
+    let fz = factorial z
     fn / (fx * fy * fz)
-    //(factorial n) / ((factorial x) * (factorial y) * (factorial (n - x)))
+
+let calcCell n x y =
+    calc n (n - x - y) y x
 
 let calcRow n len =
     List.init len (fun i -> calcCell n i (n - (len - 1)))
 
 let calcLayer n =
-    //let layers = [(n + 1) .. -1 .. 1]
     List.map (fun l -> 
         calcRow n l
     ) [1 .. (n + 1)]
 
-let printlayer n =
+let printLayer n =
     let layer = calcLayer n
-    
-    let findLongest =
-        calcCell n (n / 2 / 2) (n / 2 / 2)
-//        let row = (List.length layer) - (int (ceil (float (n + 1) / 2.0))) + 1
-//        let rowLen = float (List.length layer.[row])
-//        //let cell = int (System.Math.Round((rowLen / 2.0),0, System.MidpointRounding.))
-//        let cell = int (ceil (rowLen / 2.0))
-//        layer.[row].[cell]
-    
-    findLongest
+    let highestValue =
+        let x, y, z =
+            let y = n / 3
+            let x = (n - y) / 2
+            let z = (n - y) - x
+            (x, y, z)
+        calc n x y z
+    let width = highestValue.ToString().Length + 1
+    List.iter (fun layer ->
+        List.iter (fun cell -> printf "%*s" width (cell.ToString())) layer
+        printfn ""
+    ) (calcLayer n)
 
 [<EntryPoint>]
 let main argv = 
-//    Seq.iter (fun layer -> 
-//        Seq.iter (printf "%A ") layer
-//        printfn ""
-//    ) (calcLayer 5)
-    //let a = printlayer 1
-    //let b = printlayer 2
-    let c = printlayer 3
-    let d = printlayer 4
-    let e = printlayer 5
+    printLayer 11
+    System.Console.ReadLine() |> ignore
     0 // return an integer exit code
