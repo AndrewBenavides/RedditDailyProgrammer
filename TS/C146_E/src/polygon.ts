92194d2393ff5122bbd3ec6c190d39b0138dd4ca
Andrew Benavides <andrew.benavides@gmail.com>
2014-04-15 15:53:42 -0400

    [C146_E] Formatting changes
    - Crated resizeSvgContainer function
    - Added basic formatting to polygon
    - Polygon size is now scaled logarithmically

--- TS/C146_E/src/polygon.ts
+++ TS/C146_E/src/polygon.ts
@@ -28,25 +28,37 @@ function calculatePermiter(sides: number, length: number): number {
 }
 
 function drawPolygon(edges: number, radius: number) {
-    polygon.animate(500).ngon({
-        radius: radius,
-        edges: edges
-    }).during(function () {
-        draw.size(polygon.width(), polygon.height());
-    }).after(function () {
-        draw.size(polygon.width(), polygon.height());
-    });
+    polygon
+        .animate(500)
+        .ngon({
+            radius: (20 * Math.log(radius + 2)),
+            edges: edges
+        })
+        .during(function () {
+            resizeSvgContainer();
+        })
+        .after(function () {
+            resizeSvgContainer();
+        });
 }
 
-var draw;
+function resizeSvgContainer() {
+    svgElement.size(polygon.width() + 2, polygon.height() + 2);
+}
+
+var svgElement;
 var polygon;
 
 $(document).ready(function () {
     $("#polygonForm").submit(processForm);
     $("#polygonContainer").hide();
-    draw = SVG("polygon");
-    polygon = draw.polygon().ngon({
-        radius: 0,
-        edges: 0
-    });
+    svgElement = SVG("polygon");
+    polygon = svgElement
+        .polygon()
+        .fill("none")
+        .stroke({ width: 2 })
+        .ngon({
+            radius: 0,
+            edges: 0
+        });
 });
