ea4c01a11b3ce27a469eb3c0237d461c41268b0a
Andrew Benavides <andrew@benavides.io>
2016-07-10 12:10:03 -0400

    Work in progress for CS_C274_E.

---
+++ CS/C274_E/BealeCipher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace C274_E {
+    public class BealeCipher {
+        public static void CorrectValues(IList<string> values) {
+            values.Insert(0, string.Empty);
+            values.Insert(155, "a");
+            values.RemoveAt(242);
+            for (int i = 0; i < 10; i++) values.RemoveAt(480);
+            values.RemoveAt(620);
+            values.RemoveAt(666);
+            values[810] = "y";
+            values[1004] = "x";
+        }
+
+        public static List<string> LoadValues(string dictionaryPath) {
+            using (var file = new StreamReader(dictionaryPath))
+                return file.ReadToEnd()
+                    .Split(' ')
+                    .ToList();
+        }
+
+        public static List<int> LoadCiphers(string cipherPath) {
+            using (var file = new StreamReader(cipherPath)) {
+                return file.ReadToEnd()
+                    .Split(',')
+                    .Select(c => int.Parse(c))
+                    .ToList();
+            }
+        }
+
+        public static string Decipher(string dictionaryPath, string cipherPath) {
+            var values = LoadValues(dictionaryPath);
+            CorrectValues(values);
+            var ciphers = LoadCiphers(cipherPath);
+            var deciphered = string.Join(string.Empty, ciphers.Select(c => {
+                var v = values[c];
+                return v.First();
+            }));
+            return deciphered;
+        }
+    }
+}
---
+++ CS/C274_E/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C274_E {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(BealeCipher.Decipher(@".\Resources\DoI.txt", @".\Resources\Cipher.txt"));
+            Console.ReadLine();
+        }
+    }
+}
