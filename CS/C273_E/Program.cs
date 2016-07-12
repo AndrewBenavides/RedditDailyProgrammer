275d2da1edc6bce410514b20d0e3a30829742da7
Andrew Benavides <andrew@benavides.io>
2016-07-12 15:28:50 -0400

    Moved InputParser out of Program.cs.

--- CS/C273_E/Program.cs
+++ CS/C273_E/Program.cs
@@ -20,38 +20,4 @@ namespace C273_E {
             throw new NotImplementedException();
         }
     }
-
-    public class InputParser {
-        public string Input { get; set; }
-
-        public InputParser(string input) {
-            Input = input;
-        }
-
-        public string Process() {
-            var validation = Validate();
-            if (!string.IsNullOrWhiteSpace(validation)) return validation;
-
-            var outputUnit = Input.Substring(Input.Length - 1, 1).SingleOrDefault();
-            var inputUnit = Input.Substring(Input.Length - 2, 1).SingleOrDefault();
-            var inputValue = decimal.Parse(Input.Substring(0, Input.Length - 2));
-            var input = Unit.Create(inputUnit, inputValue);
-            var output = input.ConvertTo(outputUnit);
-            return output.Value.ToString();
-        }
-
-        public string Validate() {
-            if (string.IsNullOrWhiteSpace(Input)) return "Input is empty or white space.";
-            if (Input
-                .AsEnumerable()
-                .Count(c => char.IsLetter(c)) < 2) return "Not enough conversion unit parameters";
-            if (Input
-                .AsEnumerable()
-                .Count(c => char.IsLetter(c)) > 2) return "Too many conversion unit parameters.";
-            if (Input
-                .AsEnumerable()
-                .Count(c => char.IsNumber(c)) < 1) return "No amount specified.";
-            return string.Empty;
-        }
-    }
 }
