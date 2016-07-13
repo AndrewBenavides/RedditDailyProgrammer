6a528a8a9e57eb839b0038badf80cfde1819cb66
Andrew Benavides <andrew@benavides.io>
2016-07-12 22:43:41 -0400

    Added new Units: Celcius, Fahrenheit, Kelvin.

---
+++ CS/C273_E/Units/Fahrenheit.cs
@@ -0,0 +1,23 @@
+﻿namespace C273_E.Units {
+    public class Fahrenheit : Unit {
+        public override char Code { get; } = 'f';
+
+        public Fahrenheit(decimal value) : base(value) {
+        }
+
+        [ConversionMethod]
+        public Celcius ConvertToCelcius() {
+            return Unit.Create<Celcius>((Value - 32) * (5M / 9M));
+        }
+
+        [ConversionMethod]
+        public Fahrenheit ConvertToFahrenheit() {
+            return this;
+        }
+
+        [ConversionMethod]
+        public Kelvin ConvertToKelvin() {
+            return Unit.Create<Kelvin>((Value + 459.67M) * (5M / 9M));
+        }
+    }
+}
--- CS/C273_E/Units/Radian.cs
+++ CS/C273_E/Units/Radian.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C273_E.Units {
     public class Radian : Unit {
