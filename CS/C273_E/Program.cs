61a30383570d4a89b5698e2252926fad589daa27
Andrew Benavides <andrew@benavides.io>
2016-07-12 21:46:54 -0400

    Fixed Program loop, no longer validates before quitting.
    - Cleaned up usings
    - Removed ConvertTo method

--- CS/C273_E/Program.cs
+++ CS/C273_E/Program.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C273_E {
     class Program {
         static void Main(string[] args) {
             string input;
+            Console.WriteLine(@"Input ""quit"" to terminate the program.");
             do {
                 Console.Write("Input conversion parameters: ");
                 input = Console.ReadLine();
+                if (input == "quit") break;
                 var parser = new InputParser(input);
                 Console.WriteLine(parser.Process());
-            } while (input != "exit");
-        }
-
-        public void ConvertTo<TIn, TOut>(TIn input, TOut output) {
-            throw new NotImplementedException();
+            } while (true);
         }
     }
 }
